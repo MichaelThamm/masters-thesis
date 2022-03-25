Important Improvements and fixes to Canvas scaling

LIM/Compute.py
@@ -15,43 +15,44 @@ class Model(Grid):
         if buildFromJson:
             for attr_key in kwargs:
                 self.__dict__[attr_key] = kwargs[attr_key]
-        else:
-            super().__init__(kwargs)
+            return
 
-            self.writeErrorToDict(key='name',
-                                  error=Error.buildFromScratch(name='meshDensityDiscrepancy',
-                                                               description="ERROR - The last slot has a different mesh density than all other slots",
-                                                               cause=kwargs['meshIndexes'][0][2] != kwargs['meshIndexes'][0][-3]))
+        super().__init__(kwargs)
 
-            self.errorTolerance = kwargs['errorTolerance']
+        self.writeErrorToDict(key='name',
+                              error=Error.buildFromScratch(name='meshDensityDiscrepancy',
+                                                           description="ERROR - The last slot has a different mesh density than all other slots",
+                                                           cause=kwargs['meshIndexes'][0][2] != kwargs['meshIndexes'][0][-3]))
 
-            self.currColCount = 0
-            self.nLoop = 0
-            self.matBCount = 0
+        self.errorTolerance = kwargs['errorTolerance']
 
-            lenUnknowns = self.hmRegionsIndex[-1]
+        self.currColCount = 0
+        self.nLoop = 0
+        self.matBCount = 0
 
-            self.matrixA = np.zeros((lenUnknowns, lenUnknowns), dtype=np.cdouble)
-            self.matrixB = np.zeros(len(self.matrixA), dtype=np.cdouble)
-            self.matrixX = np.zeros(len(self.matrixA), dtype=np.cdouble)
+        lenUnknowns = self.hmRegionsIndex[-1]
 
-            self.hmUnknownsList = {i: Region.buildFromScratch(index=i, type=self.hmRegions[i], an=np.zeros(len(self.n)), bn=np.zeros(len(self.n))) for i in self.hmRegions}
+        self.matrixA = np.zeros((lenUnknowns, lenUnknowns), dtype=np.cdouble)
+        self.matrixB = np.zeros(len(self.matrixA), dtype=np.cdouble)
+        self.matrixX = np.zeros(len(self.matrixA), dtype=np.cdouble)
 
-            # HM and MEC unknown indexes in matrix A, used for visualization
-            # TODO I could make these lists more robust
-            # self.canvasRowRegIdxs = [len(self.n), len(self.n) + self.mecRegionLength,
-            #                                self.mecRegionLength + 2 * len(self.n), self.mecRegionLength + 4 * len(self.n),
-            #                                self.mecRegionLength + 6 * len(self.n), self.mecRegionLength + 8 * len(self.n)]
-            # self.canvasColRegIdxs = [len(self.n), len(self.n) + self.mecRegionLength,
-            #                                self.mecRegionLength + 3 * len(self.n), self.mecRegionLength + 5 * len(self.n),
-            #                                self.mecRegionLength + 7 * len(self.n), self.mecRegionLength + 8 * len(self.n)]
-            # self.mecCanvasRegIdxs = [self.canvasRowRegIdxs[0] + self.ppL * i for i in range(1, self.ppHeight)]
+        self.hmUnknownsList = {i: Region.buildFromScratch(index=i, type=self.hmRegions[i], an=np.zeros(len(self.n)), bn=np.zeros(len(self.n))) for i in self.hmRegions}
 
-            self.mecIdxs = list(itertools.chain.from_iterable([list(range(i, i + self.mecRegionLength)) for i in self.mecRegionsIndex]))
-            self.hmIdxs = [i for i in range(len(self.matrixX)) if i not in self.mecIdxs]
+        # HM and MEC unknown indexes in matrix A, used for visualization
+        # TODO I could make these lists more robust
+        # self.canvasRowRegIdxs = [len(self.n), len(self.n) + self.mecRegionLength,
+        #                                self.mecRegionLength + 2 * len(self.n), self.mecRegionLength + 4 * len(self.n),
+        #                                self.mecRegionLength + 6 * len(self.n), self.mecRegionLength + 8 * len(self.n)]
+        # self.canvasColRegIdxs = [len(self.n), len(self.n) + self.mecRegionLength,
+        #                                self.mecRegionLength + 3 * len(self.n), self.mecRegionLength + 5 * len(self.n),
+        #                                self.mecRegionLength + 7 * len(self.n), self.mecRegionLength + 8 * len(self.n)]
+        # self.mecCanvasRegIdxs = [self.canvasRowRegIdxs[0] + self.ppL * i for i in range(1, self.ppHeight)]
 
-            self.hmMatrixX = []
-            self.mecMatrixX = []
+        self.mecIdxs = list(itertools.chain.from_iterable([list(range(i, i + self.mecRegionLength)) for i in self.mecRegionsIndex]))
+        self.hmIdxs = [i for i in range(len(self.matrixX)) if i not in self.mecIdxs]
+
+        self.hmMatrixX = []
+        self.mecMatrixX = []
 
     @classmethod
     def buildFromScratch(cls, **kwargs):

LIM/Grid.py
@@ -30,6 +30,8 @@ class Grid(LimMotor):
 
         super().__init__(kwargs['slots'], kwargs['poles'], kwargs['length'])
 
+        self.invertY = kwargs['invertY']
+
         xMeshIndexes = kwargs['meshIndexes'][0]
         yMeshIndexes = kwargs['meshIndexes'][1]
 
@@ -321,7 +323,8 @@ class Grid(LimMotor):
                 else:
                     delX = self.xMeshSizes[c]
 
-                self.matrix[a][b] = Node.buildFromScratch(iIndex=[b, a], iXinfo=[xCnt, delX], iYinfo=[yCnt, delY], modelDepth=self.D)
+                self.matrix[a][b] = Node.buildFromScratch(iIndex=[b, a], iXinfo=[xCnt, delX], iYinfo=[yCnt, delY],
+                                                          modelDepth=self.D)
 
                 # Keep track of the x coordinate for each node
                 if b in self.xFirstEdgeNodes:
@@ -657,7 +660,7 @@ class Grid(LimMotor):
             print(f'flag - yoke: {diffYokeDims}')
             spatialDomainFlag = True
         # Check Vacuum Upper
-        diffVacUpperDims = self.vac - (self.matrix[-1][xIdx].y + self.matrix[-1][xIdx].ly - self.matrix[self.yIndexesVacUpper[0]][xIdx].y)
+        diffVacUpperDims = self.vac - (self.matrix[self.yIndexesVacUpper[-1]][xIdx].y + self.matrix[self.yIndexesVacUpper[-1]][xIdx].ly - self.matrix[self.yIndexesVacUpper[0]][xIdx].y)
         if round(diffVacUpperDims, 12) != 0:
             print(f'flag - vacuum upper: {diffVacUpperDims}')
             spatialDomainFlag = True
@@ -677,52 +680,53 @@ class Node(object):
                     self.__dict__[attr_key] = rebuildPlex(kwargs[attr_key])
                 else:
                     self.__dict__[attr_key] = kwargs[attr_key]
-        else:
-            self.xIndex = kwargs['iIndex'][0]
-            self.yIndex = kwargs['iIndex'][1]
+            return
 
-            # Initialize dense meshing near slot and teeth edges in x direction
-            self.x = kwargs['iXinfo'][0]
-            self.lx = kwargs['iXinfo'][1]
+        self.xIndex = kwargs['iIndex'][0]
+        self.yIndex = kwargs['iIndex'][1]
 
-            # Initialize dense meshing near slot and teeth edges in y direction
-            self.y = kwargs['iYinfo'][0]
-            self.ly = kwargs['iYinfo'][1]
+        # Initialize dense meshing near slot and teeth edges in x direction
+        self.x = kwargs['iXinfo'][0]
+        self.lx = kwargs['iXinfo'][1]
 
-            # Cross sectional area
-            self.Szy = self.ly*kwargs['modelDepth']
-            self.Sxz = self.lx*kwargs['modelDepth']
+        # Initialize dense meshing near slot and teeth edges in y direction
+        self.y = kwargs['iYinfo'][0]
+        self.ly = kwargs['iYinfo'][1]
 
-            self.xCenter = self.x + self.lx/2
-            self.yCenter = self.y + self.ly/2
+        # Cross sectional area
+        self.Szy = self.ly*kwargs['modelDepth']
+        self.Sxz = self.lx*kwargs['modelDepth']
 
-            # Node properties
-            self.ur = 0.0
-            self.sigma = 0.0
-            self.material = ''
-            self.colour = ''
+        self.xCenter = self.x + self.lx / 2
+        self.yCenter = self.y + self.ly / 2
 
-            # Potential
-            self.Yk = np.cdouble(0.0)
+        # Node properties
+        self.ur = 0.0
+        self.sigma = 0.0
+        self.material = ''
+        self.colour = ''
 
-            # Thrust
-            self.Fx, self.Fy, self.F = np.zeros(3, dtype=np.cdouble)
+        # Potential
+        self.Yk = np.cdouble(0.0)
 
-            # B field
-            self.Bx, self.By, self.B, self.BxLower, self.ByLower, self.B_Lower = np.zeros(6, dtype=np.cdouble)
+        # Thrust
+        self.Fx, self.Fy, self.F = np.zeros(3, dtype=np.cdouble)
 
-            # Flux
-            self.phiXp, self.phiXn, self.phiYp, self.phiYn, self.phiX, self.phiY, self.phi = np.zeros(7, dtype=np.cdouble)
-            self.phiError = np.cdouble(0.0)
+        # B field
+        self.Bx, self.By, self.B, self.BxLower, self.ByLower, self.B_Lower = np.zeros(6, dtype=np.cdouble)
 
-            # Current
-            self.Iph = np.cdouble(0.0)  # phase
+        # Flux
+        self.phiXp, self.phiXn, self.phiYp, self.phiYn, self.phiX, self.phiY, self.phi = np.zeros(7, dtype=np.cdouble)
+        self.phiError = np.cdouble(0.0)
 
-            # MMF
-            self.MMF = np.cdouble(0.0)  # AmpereTurns
+        # Current
+        self.Iph = np.cdouble(0.0)  # phase
 
-            # Reluctance
-            self.Rx, self.Ry = np.zeros(2, dtype=np.float64)
+        # MMF
+        self.MMF = np.cdouble(0.0)  # AmpereTurns
+
+        # Reluctance
+        self.Rx, self.Ry = np.zeros(2, dtype=np.float64)
 
     @classmethod
     def buildFromScratch(cls, **kwargs):
@@ -783,11 +787,12 @@ class Region(object):
                     self.__dict__[key] = valArray
                 else:
                     self.__dict__[key] = kwargs[key]
-        else:
-            self.index = kwargs['index']
-            self.type = kwargs['type']
-            self.an = kwargs['an']
-            self.bn = kwargs['bn']
+            return
+
+        self.index = kwargs['index']
+        self.type = kwargs['type']
+        self.an = kwargs['an']
+        self.bn = kwargs['bn']
 
     @classmethod
     def buildFromScratch(cls, **kwargs):

LIM/Show.py
@@ -2,6 +2,7 @@ from LIM.Compute import *
 from tkinter import *
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from CanvasModel import CanvasInFrame
 
 
 # class GUI:
@@ -23,8 +24,6 @@ def myColourNumber(fieldScale, val):
 
 
 def determineColour(jsonObject, iI, iJ, field, highlightZeroValsInField):
-
-    model = jsonObject.rebuiltModel
 
     fieldType, iFieldsScale, iStoColours, iPosInf, iNegInf = field
 
@@ -233,28 +232,31 @@ def visualizeMatrix(dims, model, bShowA=False, bShowB=False):
     mGrid.mainloop()
 
 
-def showModel(jsonObject, fieldType, showGrid, showFields, showFilter, showMatrix, showZeros, numColours, dims):
+def showModel(jsonObject, fieldType, showGrid, showFields, showFilter, showMatrix, showZeros, numColours, dims, invertY):
+
+    invertCoeff = -1 if invertY else 1
 
     # Create the grid canvas to display the grid mesh
     if showGrid:
-        rootGrid = Tk()
-        cGrid: Canvas = Canvas(rootGrid, height=dims[0], width=dims[1], bg='gray30')
-        cGrid.pack()
+
+        cGrid = CanvasInFrame(height=dims[0], width=dims[1], bg='gray30')
         i, j = 0, 0
         while i < jsonObject.rebuiltModel.matrix.shape[0]:
             while j < jsonObject.rebuiltModel.matrix.shape[1]:
-                jsonObject.rebuiltModel.matrix[i, j].drawNode(canvasSpacing=jsonObject.rebuiltModel.Cspacing, overRideColour=False, c=cGrid, nodeWidth=1)
+                jsonObject.rebuiltModel.matrix[i, j].drawNode(canvasSpacing=jsonObject.rebuiltModel.Cspacing,
+                                                              overRideColour=False, c=cGrid.canvas, nodeWidth=1)
                 j += 1
             j = 0
             i += 1
-        rootGrid.mainloop()
+
+        cGrid.canvas.scale("all", 0, 0, 1, invertCoeff)
+        cGrid.canvas.configure(scrollregion=cGrid.canvas.bbox("all"))
+        cGrid.root.mainloop()
 
     # Color nodes based on node values
     if showFields or showFilter:
 
-        rootFields = Tk()
-        cFields: Canvas = Canvas(rootFields, height=dims[0], width=dims[1], bg='gray30')
-        cFields.pack()
+        cFields = CanvasInFrame(height=dims[0], width=dims[1], bg='gray30')
 
         c1 = '#FFF888'  # Yellow Positive Limit
         c2 = '#700000'  # Dark Red Negative Limit
@@ -295,7 +297,7 @@ def showModel(jsonObject, fieldType, showGrid, showFields, showFilter, showMatri
             fieldsScale = np.arange(minScale, maxScale + normScale, normScale)
             colorScaleIndex = np.where(fieldsScale == fieldsScale[0])
 
-            cFields.create_text(400, 1000, font="Purisa", text=f"Debug (Max, Min): ({maxScale}, {minScale}) colour: ({stoColours[-1]}, {stoColours[colorScaleIndex[0][0]]}) Type: {fieldType}")
+            cFields.canvas.create_text(400, 1000, font="Purisa", text=f"Debug (Max, Min): ({maxScale}, {minScale}) colour: ({stoColours[-1]}, {stoColours[colorScaleIndex[0][0]]}) Type: {fieldType}")
             print(f"Debug (Max, Min): ({maxScale}, {minScale}) colour: ({stoColours[-1]}, {stoColours[colorScaleIndex[0][0]]}) Type: {fieldType}")
 
             # All drawing is done at the bottom of the node
@@ -352,13 +354,17 @@ def showModel(jsonObject, fieldType, showGrid, showFields, showFilter, showMatri
                                                          [fieldType, fieldsScale, stoColours, cPosInf, cNegInf],
                                                          highlightZeroValsInField=showZeros)
 
-                    jsonObject.rebuiltModel.matrix[i, j].drawNode(canvasSpacing=jsonObject.rebuiltModel.Cspacing, overRideColour=overRideColour, c=cFields, nodeWidth=1)
+                    jsonObject.rebuiltModel.matrix[i, j].drawNode(canvasSpacing=jsonObject.rebuiltModel.Cspacing,
+                                                                  overRideColour=overRideColour, c=cFields.canvas,
+                                                                  nodeWidth=1)
                     j += 1
                     k += 1
                 j = 0
                 i += 1
 
-        rootFields.mainloop()
+        cFields.canvas.scale("all", 0, 0, 1, invertCoeff)
+        cFields.canvas.configure(scrollregion=cFields.canvas.bbox("all"))
+        cFields.root.mainloop()
 
     if showMatrix:
         visualizeMatrix(dims, jsonObject.rebuiltModel, bShowA=True)

LIM/SlotPoleCalculation.py
@@ -141,9 +141,10 @@ class TransformedDict(MutableMapping):
             self.store = dict()
             for key, error in kwargs.items():
                 self.__setitem__(key, Error.buildFromJson(error))
-        else:
-            self.store = dict()
-            self.update(dict(kwargs))  # use the free update to set keys
+            return
+
+        self.store = dict()
+        self.update(dict(kwargs))  # use the free update to set keys
 
     @classmethod
     def buildFromScratch(cls, **kwargs):
@@ -197,13 +198,14 @@ class Error(object):
         if buildFromJson:
             for key in kwargs:
                 self.__dict__[key] = kwargs[key]
-        else:
-            self.name = kwargs['name']
-            self.description = kwargs['description']
-            self.cause = bool(kwargs['cause'])  # This was done to handle np.bool_ not being json serializable
-            self.state = False
+            return
 
-            self.setState()
+        self.name = kwargs['name']
+        self.description = kwargs['description']
+        self.cause = bool(kwargs['cause'])  # This was done to handle np.bool_ not being json serializable
+        self.state = False
+
+        self.setState()
 
     @classmethod
     def buildFromScratch(cls, **kwargs):